1c058575f75898257cf7a266108ceeea4cc1372a
Jaime Hernández <jaimevazz@gmail.com>
2016-12-12 13:17:25 -0600

    CSS aims and localization
    + CSS floating animation.
    - Localization portuguese.

--- Countdown/js/App.js
+++ Countdown/js/App.js
@@ -1,6 +1,12 @@
 // Date(year, month, day, hours, minutes, seconds, milliseconds)
 var dDate = new Date(2016, 11, 25, 16, 30, 0, 0);
 var counter = document.getElementById('counter');
+countdown.setLabels(
+	' milissegundo| segundo| minuto| hora| dia| semana| mês| ano| década| século| milênio',
+	' milissegundos| segundos| minutos| horas| dias| semanas| meses| anos| décadas| séculos| milênios',
+	' e ',
+	' + ',
+	'agora');
 var request;
 var timespan;
 
