e2650707690c7009173065c07840b3c716413490
Jaime Hernández <jaimevazz@gmail.com>
2017-03-24 17:54:24 -0600

    New countdown date!!
    - Changed date
    - Removed months (days only)
    + Added gradient
    + Added outline

--- Countdown/js/App.js
+++ Countdown/js/App.js
@@ -1,17 +1,17 @@
 // Date(year, month, day, hours, minutes, seconds, milliseconds)
-var dDate = new Date(2016, 11, 25, 16, 30, 0, 0);
+var dDate = new Date(2017, 05, 27, 16, 30, 0, 0);
 var counter = document.getElementById('counter');
 countdown.setLabels(
-	' milissegundo| segundo| minuto| hora| dia| semana| mês| ano| década| século| milênio',
-	' milissegundos| segundos| minutos| horas| dias| semanas| meses| anos| décadas| séculos| milênios',
-	' e ',
-	' + ',
-	'agora');
+    ' milissegundo| segundo| minuto| hora| dia| semana| mês| ano| década| século| milênio',
+    ' milissegundos| segundos| minutos| horas| dias| semanas| meses| anos| décadas| séculos| milênios',
+    ' e ',
+    ' + ',
+    'agora');
 var request;
 var timespan;
 
 function update () {
-    timespan = countdown(dDate);
+    timespan = countdown(dDate, null, countdown.DAYS|countdown.HOURS|countdown.MINUTES|countdown.SECONDS );
     counter.innerHTML = timespan.toString();
     //console.log(timespan.toString());
     request = window.requestAnimationFrame(update);
