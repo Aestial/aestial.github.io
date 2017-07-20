888148e12f5c90718cf58c848db0f2241c341496
Jaime Hernández <jaimevazz@gmail.com>
2017-07-20 13:26:40 +0100

    Radian: Back-end working
    + Added PHP script to send mail by server.
    + Added Contact form submit and Ajax communication.
    - Contact form adapted.
    - Modified CSS to new form, placeholder.
    - Corrected bugs.

--- Radian/js/main.js
+++ Radian/js/main.js
@@ -124,14 +124,12 @@ function fullpage_init() {
 			//console.log("afterResize");
 		},
 		afterLoad: function(anchorLink, index){
-			//console.log('===============');
 			//console.log("afterLoad--" + "anchorLink: " + anchorLink + " index: " + index );
 			if (isLoaded) {
 				TriggerAnim(index);
 			}
 		}
 	});
-	$('#fullpage').fullpage.setAllowScrolling(true);
 //});
 }
 function checkAnimType(animationName) {
@@ -183,6 +181,51 @@ $.fn.extend({
     	}
     }
 });
+
+$(function() {
+    // Get the form.
+    var form = $('#ajax-contact');
+    // Get the messages div.
+    var formMessages = $('#form-messages');
+    // Set up an event listener for the contact form.
+	$(form).submit(function(event) {
+	    // Stop the browser from submitting the form.
+	    event.preventDefault();
+	    // TODO
+	    // Serialize the form data.
+		var formData = $(form).serialize();
+		// Submit the form using AJAX.
+		$.ajax({
+		    type: 'POST',
+		    url: $(form).attr('action'),
+		    data: formData
+		}).done(function(response) {
+		    // Make sure that the formMessages div has the 'success' class.
+		    $(formMessages).removeClass('error');
+		    $(formMessages).addClass('success');
+
+		    // Set the message text.
+		    $(formMessages).text(response);
+
+		    // Clear the form.
+		    $('#name').val('');
+		    $('#email').val('');
+		    $('#message').val('');
+		}).fail(function(data) {
+		    // Make sure that the formMessages div has the 'error' class.
+		    $(formMessages).removeClass('success');
+		    $(formMessages).addClass('error');
+
+		    // Set the message text.
+		    if (data.responseText !== '') {
+		        $(formMessages).text(data.responseText);
+		    } else {
+		        $(formMessages).text('Oops! An error occured and your message could not be sent.');
+		    }
+		});
+	});
+});
+
 function toggleBigImage(index) {
 	if (!isPlaying) {
 		if (imageActive) hideBigImage();
