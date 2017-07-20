ba2f6c86c3990ed8c4bf3815d771ae7e9ebce437
Jaime Hernández <jaimevazz@gmail.com>
2017-07-20 14:07:08 +0100

    Radian: Contact fixed
    - Fixed mailer PHP to display correct name and mail in headers.
    - Fixed Contact form layout.

--- Radian/mailer.php
+++ Radian/mailer.php
@@ -4,10 +4,9 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Get the form fields and remove whitespace.
         $name = strip_tags(trim($_POST["name"]));
-				$name = str_replace(array("\r","\n"),array(" "," "),$name);
+		$name = str_replace(array("\r","\n"),array(" "," "),$name);
         $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
         $message = trim($_POST["message"]);
-
         // Check that data was sent to the mailer.
         if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
             // Set a 400 (bad request) response code and exit.
@@ -15,19 +14,16 @@
             echo "Oops! There was a problem with your submission. Please complete the form and try again.";
             exit;
         }
-
+        
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "jaime@radian.mx";
-
+        $recipient = "hola@radian.mx, heber@radian.mx, jaime@radian.mx";
         // Set the email subject.
-        $subject = "New contact from $name";
-
+        $subject = "Contacto de $name. Web RADIAN";
         // Build the email content.
-        $email_content = "Name: $name\n";
+        $email_content = "Nombre: $name\n";
         $email_content .= "Email: $email\n\n";
-        $email_content .= "Message:\n$message\n";
-
+        $email_content .= "Mensaje:\n$message\n";
         // Build the email headers.
         $email_headers = "From: $name <$email>";
 
