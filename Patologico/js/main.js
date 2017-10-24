f665cba3e2369840042a128639e4792c38793375
Jaime Hernández <jaimevazz@gmail.com>
2017-10-24 13:05:39 -0500

    Patologico update
    - One layer file

--- Patologico/js/main.js
+++ Patologico/js/main.js
@@ -1,4 +1,4 @@
-var colors= ['aqua', 'black', 'blue', 'fuchsia', 'gray', 'green',
+var colors= ['aqua', 'blue', 'fuchsia', 'gray', 'green',
 'lime', 'maroon', 'navy', 'olive', 'orange', 'purple', 'red',
 'silver', 'teal', 'white', 'yellow'];
 
@@ -8,15 +8,10 @@ window.onload = function () {
     viewBox: [0, 0, 640, 873]
   });
   Snap.load("svg/patologico.svg", function (f) {
-    //var camisa = f.select("#camisa").attr({fill: "#fff"});
     var camisa = f.select("#camisa"),
-    camisaC = f.select("#camisa-color"),
-    camisaO = f.select("#camisa-out"),
-    corbataC = f.select("#corbata-color");
+    root = f.select("#root");
     var top = g.g();
-    top.add(camisaC);
-    top.add(camisaO);
-    top.add(corbataC);
+    top.add(root);
     window.onclick = function () {
       var color = colors[Math.floor(Math.random() * colors.length)];
       camisa.attr({fill: color});
