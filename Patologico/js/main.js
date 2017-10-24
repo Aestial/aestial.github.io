8964c308857dedefbe26f5b3483e98215d31ea38
Jaime Hernández <jaimevazz@gmail.com>
2017-10-24 11:47:06 -0500

    Patologico update
    - Snap.svg

--- Patologico/js/main.js
+++ Patologico/js/main.js
@@ -1,9 +1,25 @@
 var colors= ['aqua', 'black', 'blue', 'fuchsia', 'gray', 'green',
 'lime', 'maroon', 'navy', 'olive', 'orange', 'purple', 'red',
 'silver', 'teal', 'white', 'yellow'];
-function svgMod()
-{
-  var camisa_fill_color = document.getElementById("camisa_fill");
-  console.log(camisa_fill_color);
-  camisa_fill_color.style.fill = colors[Math.floor(Math.random() * colors.length)];
-}
+
+window.onload = function () {
+  var g = Snap();
+  g.attr({
+    viewBox: [0, 0, 640, 873]
+  });
+  Snap.load("svg/patologico.svg", function (f) {
+    //var camisa = f.select("#camisa").attr({fill: "#fff"});
+    var camisa = f.select("#camisa"),
+    camisaC = f.select("#camisa-color"),
+    camisaO = f.select("#camisa-out"),
+    corbataC = f.select("#corbata-color");
+    var top = g.g();
+    top.add(camisaC);
+    top.add(camisaO);
+    top.add(corbataC);
+    window.onclick = function () {
+      var color = colors[Math.floor(Math.random() * colors.length)];
+      camisa.attr({fill: color});
+    };
+  });
+};
