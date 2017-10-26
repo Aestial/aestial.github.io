023fad4ecc1ed772a52aa3493b3547dfbc5f9673
Jaime Hernández <jaimevazz@gmail.com>
2017-10-26 07:21:13 -0500

    Patologico color elements - Change colors randomly of desired elements including child (rombos) with one click. - Correctly using Snap.svg

--- Patologico/js/main.js
+++ Patologico/js/main.js
@@ -8,13 +8,32 @@ window.onload = function () {
     viewBox: [0, 0, 640, 873]
   });
   Snap.load("svg/patologico.svg", function (f) {
-    var camisa = f.select("#camisa"),
-    root = f.select("#root");
+    var root = f.select("#root"),
+    camisa = f.select("#camisa"),
+    corbata = f.select("#corbata"),
+    rombos = f.select("#rombos");
     var top = g.g();
     top.add(root);
+    console.log(rombos.children());
     window.onclick = function () {
       var color = colors[Math.floor(Math.random() * colors.length)];
+      var color2 = colors[Math.floor(Math.random() * colors.length)];
+      var color3 = colors[Math.floor(Math.random() * colors.length)];
       camisa.attr({fill: color});
+      corbata.attr({fill: color2});
+      cambiaRombos(color3);
     };
+    function cambiaRombos (color)
+    {
+      for(var i = 0; i < rombos.children().length; i++)
+      {
+        if (rombos.children()[i].type == "rect"
+         || rombos.children()[i].type == "path")
+        {
+          console.log(rombos.children()[i].type);
+          rombos.children()[i].attr({fill: color});
+        }
+      }
+    }
   });
 };
